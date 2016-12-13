03f12805d3a923132fdcbad7b43fabc96635a3a2
Ayoub Falah <ayoub_falah@hotmail.de>
2016-12-13 18:21:59 +0100

    A managed bean that is responsible for holding the data of a comment element

---
+++ main/java/de/softunivers/blog/controllers/CommentBackingBean.java
@@ -0,0 +1,23 @@
+
+package de.softunivers.blog.controllers;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author ayoubfalah
+ */
+@Getter
+@Setter
+@Named
+@RequestScoped
+public class CommentBackingBean implements Serializable
+{
+    private Integer id;
+    private String body;
+    private Integer postId;
+}
